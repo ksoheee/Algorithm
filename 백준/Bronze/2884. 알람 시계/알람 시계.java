3d115fe3fe924f675de593fadf8d23e7fec997ba
kkksohee <83942772+kkksohee@users.noreply.github.com>
2025-06-10 22:24:54 +0900

    [Bronze III] Title: 알람 시계, Time: 192 ms, Memory: 18504 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,17 +1,13 @@
 import java.util.Scanner;
-
-public class Main {
+public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int hour = sc.nextInt(); //공백기준
-        int min = sc.nextInt();
-        int calc = hour *60 +min; //분으로 환산
-        if(calc <45){
-            calc = 24 *60+min; //45미만에 대한 처리
-        }calc -=45;
-
-        System.out.println((calc/60)+" "+ (calc %60));
-
+        int M=sc.nextInt();
+        int N=sc.nextInt();
+        int time = M*60+N;
+        if(time<45) time=24*60+N;
+        time -=45;
+        System.out.print(time/60+" " +time%60);
 
     }
 }
