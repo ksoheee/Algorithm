442f1979e051f7fd076e3558c4c5870444d84cd5
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-07-02 08:55:59 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1780 ms, Memory: 186088 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,4 +1,3 @@
-
 import java.util.*;import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
@@ -10,14 +9,9 @@ public class Main{
             arr[i][0]=sc.nextInt();
             arr[i][1]=sc.nextInt();
         }
-        Arrays.sort(arr, new Comparator<int[]>(){
-            @Override
-            public int compare(int[] e1, int[] e2 ){
-                if(e1[0]==e2[0])
-                    return e1[1]-e2[1];
-                else
-                    return e1[0]-e2[0];
-            }
+        Arrays.sort(arr, (a1, a2)->{
+            if(a1[0]==a2[0]) return a1[1]-a2[1];
+            else return a1[0]-a2[0];
         });
         for(int i=0; i<n; i++){
             System.out.println(arr[i][0]+" "+arr[i][1]+" ");
