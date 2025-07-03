c9304e4b91bcac14fb33153bfaf82830d80330fd
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-07-03 19:29:08 +0900

    [Silver V] Title: 나이순 정렬, Time: 860 ms, Memory: 137184 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,16 +1,21 @@
-import java.util.*;import java.util.Scanner;
+import java.util.Scanner;
 public class Main{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n= sc.nextInt();
-        String[][] arr = new String[n][2];
+        
+        StringBuilder[] p = new StringBuilder[201];
+        for(int i=0; i<p.length; i++) p[i]=new StringBuilder();
+        
         for(int i=0; i<n; i++){
-            arr[i][0]=sc.next();
-            arr[i][1]=sc.next();
+            int age=sc.nextInt();
+            String name=sc.next();
+            //카운팅 정렬: 나이를 index로 하여 해당 배열에 나이와 이름을 append()한다.
+            p[age].append(age).append(" ").append(name).append("\n");
         }
-        Arrays.sort(arr, (o1,o2)->{return Integer.parseInt(o1[0])-Integer.parseInt(o2[0]);});
-        StringBuilder sbb= new StringBuilder();
-        for(int i=0; i<n; i++) sbb.append(arr[i][0]).append(' ').append(arr[i][1]).append('\n');
-        System.out.print(sbb);
+        StringBuilder sb = new StringBuilder();
+        for(StringBuilder val: p){
+            sb.append(val);
+        }System.out.println(sb);
     }
 }
