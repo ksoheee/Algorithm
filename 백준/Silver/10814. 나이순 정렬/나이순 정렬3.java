f90777c4474910df8c8f46a07d89279df5bd2680
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-07-03 12:33:11 +0900

    Create 나이순 정렬3.java

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬3.java
@@ -0,0 +1,17 @@
+//방법 3 : [String[][] + BufferedReader + StringBuilder]
+import java.util.*;import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        String[][] arr = new String[n][2];
+        for(int i=0; i<n; i++){
+            arr[i][0]=sc.next();
+            arr[i][1]=sc.next();
+        }
+        Arrays.sort(arr, (o1,o2)->{return Integer.parseInt(o1[0])-Integer.parseInt(o2[0]);});
+        StringBuilder sbb= new StringBuilder();
+        for(int i=0; i<n; i++) sbb.append(arr[i][0]).append(' ').append(arr[i][1]).append('\n');
+        System.out.print(sbb);
+    }
+}
