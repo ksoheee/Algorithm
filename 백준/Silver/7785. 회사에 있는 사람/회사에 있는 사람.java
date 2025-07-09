4459babbb0b4555340350737fba116df1c24fc1b
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-07-10 00:03:17 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 1188 ms, Memory: 53632 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,27 +1,24 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 public class Main{
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        
-        Map<String,String> map = new HashMap<String,String>();
+        int n= Integer.parseInt(br.readLine());
+        Map<String,String> map = new HashMap<>();
         for(int i=0; i<n; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-                   
-            String name = st.nextToken();
-            String state = st.nextToken();
+            StringTokenizer st= new StringTokenizer(br.readLine());
+            String name=st.nextToken();
+            String state=st.nextToken();
+            
             if(map.containsKey(name)){
                 map.remove(name);
             }else{
                 map.put(name,state);
             }
         }
-        ArrayList<String> arr = new ArrayList<String>(map.keySet());
-        Collections.sort(arr, Collections.reverseOrder());
-        for(var a:arr){
-            System.out.println(a+" ");
-        }
+        ArrayList<String> arr= new ArrayList<>(map.keySet());
+        arr.sort((o1,o2)->o2.compareTo(o1));
+        for(String st: arr) System.out.println(st);
+   
     }
 }
