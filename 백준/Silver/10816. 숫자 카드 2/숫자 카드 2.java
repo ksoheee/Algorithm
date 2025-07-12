9ac5bdd361722e626966eca3ea1c22d014dce5ab
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-07-12 12:53:47 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1000 ms, Memory: 173172 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,29 +1,28 @@
+//HashMap 2
 import java.util.*;
 import java.io.*;
 public class Main{
     public static void main(String[] args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        st=new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        
-        
-        st=new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
+        int n = Integer.parseInt(br.readLine());
         Map<Integer, Integer> map = new HashMap<>();
+        
+        StringTokenizer st= new StringTokenizer(br.readLine(), " ");
+        
         int cnt=0;
         for(int i=0; i<n; i++){
             int num=Integer.parseInt(st.nextToken());
-            if(map.containsKey(num)) map.put(num,map.get(num)+1);
-            else map.put(num, 1);
+            //getOrDefault(key, defaultValue):key에 저장된 map에 저장된 value를 반환한다.
+            //만약 없을 경우 defaultValue값을 반환한다. 
+            map.put(num, map.getOrDefault(num,0)+1);
         }
-        st=new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken());
-        st=new StringTokenizer(br.readLine());
+        
+        int m = Integer.parseInt(br.readLine());
+        st= new StringTokenizer(br.readLine(), " ");
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<m; i++){
             int num=Integer.parseInt(st.nextToken());
-            if(map.containsKey(num)) sb.append(map.get(num)).append(" ");
-            else sb.append("0 ");
+            sb.append(map.getOrDefault(num,0)).append(" ");
         }System.out.print(sb);     
     }
 }
