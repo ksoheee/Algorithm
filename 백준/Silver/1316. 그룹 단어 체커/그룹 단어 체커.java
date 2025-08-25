5437020f10dd7eeeef69b3f401902d83f90cd825
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-08-25 17:39:13 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.io.*;
 public class Main {
+    //main함수와 check함수에서도 BufferedReader를 사용하니 전역변수로 static으로 선언
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	public static void main(String[] args) throws IOException {
 		
@@ -16,22 +17,22 @@ public class Main {
     }
     public static boolean groupCheck() throws IOException {
         boolean[] alp = new boolean[26]; 
-        int prev = 0;
+        int prev = 0; //이전 값
         
         String str = br.readLine();
         
         for(int j=0; j<str.length(); j++){
             int now = str.charAt(j);
             
-            if(prev != now){
-                if(alp[now-'a']==false){
-                    alp[now-'a']=true;
-                    prev = now;
-                }else{
+            if(prev != now){  //이전 문자와 현재 문자가 다르다면 
+                if(alp[now-'a']==false){  //처음 문자가 나왔다면 
+                    alp[now-'a']=true;  //true로 변경
+                    prev = now;         //이전값을 현재 값으로 
+                }else{//문자가 다른데, 처음 나왔던 문자가 아니라면 그룹문자가 아니므로 return
                     return false;
                 }
             }
-            else{
+            else{                        //이전 문자와 같다면
                 continue;
             }
             
