2ff0d69f4d1f326137e56c52599e0532201969be
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-08-27 17:43:39 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -15,12 +15,12 @@ public class Main {
             arr[i]= Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr); //오름차순 정렬
-        int sum = 0;
-        int pre = 0;
+        int sum = 0; //사람별 대기시간 총합
+        int pre = 0; //이전까지 대기시간의 누적합
         for(int i=0; i<N; i++){
-            sum += pre + arr[i];
+            sum += pre + arr[i]; //이전까지 대기시간과 현재 대기시간을 더한다.
             
-            pre += arr[i];
+            pre += arr[i]; //이전가지 누적합에 걸리는 시간에 현재 걸리는 시간을 더한다.
         }
         System.out.println(sum);
     }
