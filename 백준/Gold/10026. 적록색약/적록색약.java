e7d8677e7482b0674031de310d2925f080a492a8
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-09-20 16:13:58 +0900

    [Gold V] Title: 적록색약, Time: 136 ms, Memory: 16508 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,90 +1,93 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    static int N;
-    static char[][] RGB;
+    static char[][] picture;
     static boolean[][] visited;
-    static boolean[][] visitedrg;
+    static int N;
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
-        RGB = new char[N][N];
-        visited= new boolean[N][N];
-        visitedrg= new boolean[N][N];
+        picture = new char[N][N];
+
         for(int i=0; i<N; i++){
-            String tmp = br.readLine();
+            String str = br.readLine();
             for(int j=0; j<N; j++){
-                RGB[i][j]= tmp.charAt(j);
+                picture[i][j]= str.charAt(j);
             }
         }
-        int np=0; int p=0;
+        int cnt=0;
+        visited = new boolean[N][N];
         for(int i=0; i<N; i++){
             for(int j=0; j<N; j++){
-                if(!visited[i][j]) {
-                    np++;
+                if(!visited[i][j]){
                     bfs(i,j);
-                }
-                if(!visitedrg[i][j]){
-                    p++;
-                    bfsRG(i,j);
+                    cnt++;
                 }
             }
         }
-        System.out.println(np+" "+p);
-
+        int cntrg=0;
+        visited = new boolean[N][N];
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                if(!visited[i][j]){
+                    bfsrg(i,j);
+                    cntrg++;
+                }
+            }
+        }
+        System.out.println(cnt+" "+cntrg);
     }
-    static void bfs(int a, int b){
+    static void bfs(int a,int b){
         Queue<int[]> q = new LinkedList<>();
+        visited[a][b] = true;
         q.offer(new int[]{a,b});
-        visited[a][b]=true;
 
         while(!q.isEmpty()){
-            int p[] = q.poll();
-            int py = p[0];
-            int px = p[1];
+            int[] pre=q.poll();
+            int py = pre[0];
+            int px = pre[1];
 
-            char str = RGB[py][px];
-
+            char cur = picture[py][px];
             for(int i=0; i<4; i++){
-                int nx = px +dx[i];
-                int ny = py +dy[i];
+                int ny = py + dy[i];
+                int nx = px + dx[i];
 
-                if(nx>=N ||ny>= N||nx<0||ny<0)continue;
-                if(visited[ny][nx]) continue;
-                if(RGB[ny][nx]!= str) continue;
-                q.offer(new int[]{ny,nx});
+                if(ny<0||nx<0||ny>=N||nx>=N) continue;
+                if(picture[ny][nx]!=cur) continue;
+                if(visited[ny][nx])continue;
                 visited[ny][nx]=true;
-
+                q.offer(new int[]{ny,nx});
             }
         }
     }
-    static void bfsRG(int a, int b){
+    static void bfsrg(int a,int b){
         Queue<int[]> q = new LinkedList<>();
+        visited[a][b] = true;
         q.offer(new int[]{a,b});
-        visitedrg[a][b]=true;
 
-        char str = RGB[a][b];
         while(!q.isEmpty()){
-            int p[] = q.poll();
-            int py = p[0];
-            int px = p[1];
+            int[] pre=q.poll();
+            int py = pre[0];
+            int px = pre[1];
+
+            char cur = picture[py][px];
 
             for(int i=0; i<4; i++){
-                int nx = px +dx[i];
-                int ny = py +dy[i];
+                int ny = py + dy[i];
+                int nx = px + dx[i];
 
-                if(nx>=N ||ny>= N||nx<0||ny<0)continue;
-                if(visitedrg[ny][nx]) continue;
-                if(str == 'B'){
-                    if(RGB[ny][nx] != 'B') continue;
-                } else {
-                    if(RGB[ny][nx] == 'B') continue;
+                if(ny<0||nx<0||ny>=N||nx>=N) continue;
+                if(cur == 'R' || cur == 'G'){
+                    if(picture[ny][nx]=='B') continue;
+                }else{
+                    if(picture[ny][nx]!='B') continue;
                 }
+                if(visited[ny][nx])continue;
+                visited[ny][nx]=true;
                 q.offer(new int[]{ny,nx});
-                visitedrg[ny][nx]=true;
             }
         }
     }
