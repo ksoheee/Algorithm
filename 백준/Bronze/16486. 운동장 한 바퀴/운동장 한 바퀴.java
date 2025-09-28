3094245831cfae56d3f10989210e1a8274e88be8
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-09-28 23:05:32 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+public class Main {   
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        
+        System.out.println((2*N)+(2*M*3.141592));
+    }
+}
