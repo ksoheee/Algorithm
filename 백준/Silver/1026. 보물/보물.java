ab194063bd19fc0f33f29b45fc9afeb1c8cc8612
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-11-18 15:36:35 +0900

    [Silver IV] Title: 보물, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -2,9 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
