f7326ab794570e2c384642ce01bf4dca5d72baf1
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-11-18 13:49:16 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 90.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int solution(String name) {
+        int answer = 0;
+        int length= name.length();
+        int minMove = length-1; //왼쪽으로만 이동했을 때
+        
+        for(int i=0; i<length; i++){
+            char alp = name.charAt(i);
+            //상하 이동
+            int minUpDown = Math.min(alp-'A','Z'-alp+1);
+            answer += minUpDown;
+            
+            //연속된 A숫자
+            int next = i+1;
+            while(next<length && name.charAt(next)=='A'){
+                next++;
+            }
+            //연속된 A를 만나면 되돌아가는것과 왼쪽으로만 가는 횟수중 최소
+            minMove = Math.min(minMove, i*2+(length-next));
+            //처음부터 돌아가서 연속된 A를 만나기 전까지 처리하고 앞으로 돌아오는 경우
+            minMove = Math.min(minMove, (length-next)*2+i);   
+        }
+        answer += minMove;
+        return answer;
+    }
+}
