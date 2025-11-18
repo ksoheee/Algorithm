233de8f39cc3d5ae21b98807d9ed62b560800fca
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-11-18 13:58:03 +0900

    Fix comments and clarify minMove calculation

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -3,7 +3,7 @@ class Solution {
     public int solution(String name) {
         int answer = 0;
         int length= name.length();
-        int minMove = length-1; //왼쪽으로만 이동했을 때
+        int minMove = length-1; //오른쪽으로만 이동했을 때
         
         for(int i=0; i<length; i++){
             char alp = name.charAt(i);
@@ -16,7 +16,7 @@ class Solution {
             while(next<length && name.charAt(next)=='A'){
                 next++;
             }
-            //연속된 A를 만나면 되돌아가는것과 왼쪽으로만 가는 횟수중 최소
+            //오른쪽으로 이동하다가 연속된 A를 만나면 되돌아가는것과 오른쪽로만 가는 횟수중 최소
             minMove = Math.min(minMove, i*2+(length-next));
             //처음부터 돌아가서 연속된 A를 만나기 전까지 처리하고 앞으로 돌아오는 경우
             minMove = Math.min(minMove, (length-next)*2+i);   
