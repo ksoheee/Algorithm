a33e68a5622fa0f15ece63cd6f2ad69b2bb41512
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-11-24 13:17:27 +0900

    Update 로또의 최고 순위와 최저순위.java

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저순위.java
@@ -1,20 +1,20 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = new int[2];
         int[] rank = {6,6,5,4,3,2,1};
         int cnt =0;
         int z =0;
-        for(int i=0; i<6; i++){
-            for(int j=0; j<6; j++){
-                if(lottos[i] == win_nums[j]){
-                    cnt++;
-                }
-            }
-            if(lottos[i]==0) z++;
+        
+        HashSet<Integer> set = new HashSet<>();
+        for(int w : win_nums){
+            set.add(w);
         }
-        answer[0] = rank[z+cnt];
-        answer[1] = rank[cnt]; 
-        return answer;
+        for(int l : lottos){
+            if(l==0) z++;
+            else if(set.contains(l)) cnt++;
+        }
+        int max = rank[z+cnt];
+        int min = rank[cnt]; 
+        return new int[]{max,min};
     }
 }
