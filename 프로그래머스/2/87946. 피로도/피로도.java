b4c8c4ff81cc9a5758c299090490043e40fa934e
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-12-02 15:13:01 +0900

    [level 2] Title: 피로도, Time: 4.15 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,40 +1,23 @@
 import java.util.*;
 class Solution {
     boolean[] visited;
-    List<List<Integer>> list = new ArrayList<>();
+    int result;
     public int solution(int k, int[][] dungeons) {
-        int len = dungeons.length;
-        visited = new boolean[len];
+        visited = new boolean[dungeons.length];
         
-        permutation(new ArrayList<>(), len);
+        permutation(k, 0, dungeons);
         
-        int result = 0;
-        
-        for(List<Integer> d : list){
-            int fatigue = k;
-            int cnt = 0;
-            for(int i : d){
-                if(dungeons[i][0]>fatigue) break;
-                fatigue -= dungeons[i][1];
-                cnt++;
-            }
-            result = Math.max(cnt, result);
-        }
         return result;
     }
     //순열의 조합
-    public void permutation(List<Integer> combo, int len){
-        if(combo.size() == len){
-            list.add(new ArrayList<>(combo));
-            return;
-        }
-        for(int i=0; i<len; i++){
-            if(!visited[i]){
+    public void permutation(int k, int cnt, int[][] dungeons){
+        result = Math.max(cnt, result);
+        
+        for(int i=0; i<dungeons.length; i++){
+            if(!visited[i] && dungeons[i][0]<=k){
                 visited[i]= true;
-                combo.add(i);
-                permutation(combo, len);
-            
-                combo.remove(combo.size()-1);
+                permutation(k-dungeons[i][1], cnt+1, dungeons);
+                
                 visited[i]= false;
             }
         }
