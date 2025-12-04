dd89fe7542d86d4b621d801e16d79354d7febf15
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-12-04 16:37:59 +0900

    [level 2] Title: 스킬트리, Time: 0.27 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,13 +1,30 @@
+import java.util.*;
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
+        int len = skill.length();
         
-        for(String tree : skill_trees){
-            String filter = tree.replaceAll("[^" +skill + "]", "");
+        List<Character> check;;
+        
+        for(int i=0; i<skill_trees.length; i++){
+            check = new ArrayList<>();
+            for(int j=0; j<skill_trees[i].length(); j++){
+                for(int k=0; k<len; k++){
+                    if(skill_trees[i].charAt(j)==skill.charAt(k)) {
+                        check.add(skill_trees[i].charAt(j));
+                    }    
+                }
+            }
             
-            if(skill.startsWith(filter)) answer++;
+            boolean flag = true;
+            for(int j=0; j<check.size(); j++){
+                if(skill.charAt(j) != check.get(j)){
+                    flag=false;
+                    break; 
+                } 
+            }
+            if(flag) answer++;
         }
         return answer;
-        
     }
 }
