13fb08936abd0400fea1df57bfd785e9f3a007f3
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-12-07 20:40:20 +0900

    [level 2] Title: 문자열 압축, Time: 3.99 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,35 +1,45 @@
 import java.util.*;
 class Solution {
     public int solution(String s) {
-        int length = s.length();
-        if(length == 1) return 1;
+        int answer = Integer.MAX_VALUE;
+        int len = s.length();
+        StringBuilder sb;
         
-        String prev = "";
-        int answer = length;
-        //압축 단위를 1~length/2까지 
-        for(int unit = 1; unit<=length/2; unit++){
-            prev = s.substring(0,unit); //첫 조각
-            int cnt = 1; int resultLen = 0;
-            int remain = length% unit;
-            
-            for(int j = unit; j+unit <= length; j+=unit){
-                if(prev.equals(s.substring(j, j+unit))) cnt ++;
+        if(len==1) return 1;
+        
+        //몇개의 갯수로 잘라야 하는지 
+        for(int i=1; i<=len/2; i++){
+            sb = new StringBuilder();
+            String pre = s.substring(0,i);
+            int cnt=1;
+            String tmp ="";
+            for(int j=i; j<=len-i; j+=i){
+                tmp = s.substring(j,j+i);
+                
+                if(pre.equals(tmp)){
+                    cnt++;
+                }
                 else{
-                    if(cnt == 1) resultLen += prev.length();
-                    else         resultLen += String.valueOf(cnt).length()+prev.length();
-                    prev = s.substring(j, j+unit);
+                    if(cnt<2)  sb.append(pre);
+                    else        sb.append(String.valueOf(cnt)).append(pre);
+                    pre = tmp;
                     cnt = 1;
                 }
+               
             }
-            //마지막 prve 처리
-            if(cnt == 1) resultLen += prev.length();
-            else         resultLen += String.valueOf(cnt).length()+prev.length();
+            //마지막 추가
+            if(cnt<2)  sb.append(tmp);
+            else        sb.append(String.valueOf(cnt)).append(tmp);
             
-            //남는 문자열 
-            resultLen += remain;
-
-            answer = Math.min(resultLen, answer);
+            //나머지 문자열
+            if(len%i!=0){
+                String r = s.substring(len-(len%i),len);
+                sb.append(r);
+            }
+            answer = Math.min(answer, sb.toString().length());
+            
         }
+        
         return answer;
     }
 }
