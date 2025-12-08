6493f719061ccb53be52775950a8d29653f9b4c4
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-12-08 18:21:48 +0900

    [level 3] Title: 표 편집, Time: 854.27 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,62 +1,53 @@
 import java.util.*;
 class Solution {
     public String solution(int n, int k, String[] cmd) {
-        int[] pre = new int[n];
-        int[] next = new int[n];
+        int len = cmd.length;
         
-        //초기값
-        for(int i=0; i<n; i++){
-            pre[i] = i-1;
-            next[i] = i+1;
-        }
-        next[n-1] = -1;
         //삭제된 행을 저장할 스택
-        Deque<int[]> stack = new ArrayDeque<>();
-        boolean[] deleted = new boolean[n];
+        Deque<Integer> saveDelete = new ArrayDeque<>();
+        List<String> result = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            result.add("O");
+        }
         //현재 위치
-        int idx = k;
+        int idx = k; int r = n;
         
-        for(int i=0; i<cmd.length; i++){
-            char ch = cmd[i].charAt(0);
-            if(ch == 'D'){
-                int t = Integer.parseInt(cmd[i].substring(2));
-                for(int j=0; j<t; j++) idx = next[idx];
-            }else if(ch == 'U'){
-                int t = Integer.parseInt(cmd[i].substring(2));
-                for(int j=0; j<t; j++) idx = pre[idx];
-            }
-            else if(ch == 'C'){
-                deleted[idx]= true;
-                stack.push(new int[]{idx, pre[idx], next[idx]});
-                
-                //현재 행을 제거
-                if(pre[idx]!=-1)    next[pre[idx]] = next[idx];
-                if(next[idx]!=-1)   pre[next[idx]] = pre[idx];
-                
-                //커서 이동
-                if(next[idx] != -1) idx = next[idx];
-                else                idx = pre[idx];
-                
-            }else if(ch == 'Z'){
-                int[] re = stack.pop();
-                int reIdx = re[0];
-                int reP = re[1];
-                int reN = re[2];
-                deleted[reIdx]= false;
-                
-                if(reP != -1)   next[reP] = reIdx;
-                if(reN != -1)   pre[reN] = reIdx;
-                
-                pre[reIdx] = reP;
-                next[reIdx] = reN;
+        for(int i=0; i<len; i++){
+            String tmp = cmd[i];
+            //1글자일때
+            if(tmp.length()==1){
+               if(tmp.equals("Z")){
+                   //스택에서 불러오고 행 길이 늘림
+                   int d = saveDelete.pop();
+                   if(idx>=d) idx++;
+                   r++;
+               }else if(tmp.equals("C")){
+                   //삭제한 행위위치 저장
+                   saveDelete.push(idx);
+                   r--;
+                   //삭제하면 전체 길이 -1
+                   //만약에 삭제 하고나서 길이를 줄였는데 인덱스가 길이와 같아지면 현재인덱스-1
+                   if(idx==r) idx--;
+               }
+            }else{
+                String[] arr = tmp.split(" ");
+                if(arr[0].equals("U")){
+                    idx -= Integer.parseInt(arr[1]);
+                    if(idx<0) idx = 0;
+                }else if(arr[0].equals("D")){
+                    idx += Integer.parseInt(arr[1]);
+                    if(idx>r-1) idx = r-1;
+                }
             }
         }
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<n; i++){
-            if(deleted[i])  sb.append("X");
-            else            sb.append("O");
+        for(int i=0; i<r; i++){
+            sb.append("O");
         }
-       return sb.toString();
+        while(!saveDelete.isEmpty()){//삭제된게 있으면
+            sb.insert(saveDelete.pop(),"X");
+        }
+        return sb.toString();
     }
     
 }
