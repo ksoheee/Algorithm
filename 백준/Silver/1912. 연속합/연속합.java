3e332dabe5c95e484b38e6653c684dca970b6fe7
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-12-11 20:42:21 +0900

    [Silver II] Title: 연속합, Time: 224 ms, Memory: 23308 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n+1];
+        for(int i=1; i<=n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[]dp = new int[n+1];
+        int max = arr[1];
+        dp[1]= arr[1];
+        for(int i=2; i<=n; i++){
+            dp[i] = Math.max(dp[i-1]+arr[i], arr[i]);
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+}
