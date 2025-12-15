f872778c28d4c46b2f2c160feab89081f4a6d373
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-12-15 13:54:52 +0900

    [Gold I] Title: 열쇠, Time: 184 ms, Memory: 21716 KB -BaekjoonHub

--- 백준/Gold/9328. 열쇠/열쇠.java
+++ 백준/Gold/9328. 열쇠/열쇠.java
@@ -1,111 +1,101 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-
-    static int h, w;
-    static char[][] map;
+class Main{
+    static String keys;
+    static char[][] arr;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int n,m;
+    static int cnt;
     static boolean[][] visited;
     static boolean[] hasKey;
-    static List<int[]>[] doors;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] agrs)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            arr = new char[n+2][m+2];
 
-        while (T-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            h = Integer.parseInt(st.nextToken());
-            w = Integer.parseInt(st.nextToken());
-
-            //외부 패딩
-            map = new char[h + 2][w + 2];
-            for (int i = 0; i < h + 2; i++) {
-                Arrays.fill(map[i], '.');
-            }
-
-            for (int i = 1; i <= h; i++) {
-                String line = br.readLine();
-                for (int j = 1; j <= w; j++) {
-                    map[i][j] = line.charAt(j - 1);
+            //맵 한칸 확장 (j=0, j=n+1 k=0, k=m+1)
+            for (int j = 0; j <= n+1; j++) {
+                for (int k = 0; k <= m+1; k++) {
+                    arr[j][k] = '.';
                 }
             }
 
+            for (int j = 1; j <= n; j++) {
+                String tmp = br.readLine();
+                for (int k = 1; k <= m; k++) {
+                    arr[j][k] = tmp.charAt(k-1);
+                }
+            }
             hasKey = new boolean[26];
-            String keyLine = br.readLine();
-            if (!keyLine.equals("0")) {
-                for (char c : keyLine.toCharArray()) {
-                    hasKey[c - 'a'] = true;
+            keys = br.readLine();
+            if (!keys.equals("0")){
+                for(char c : keys.toCharArray()){
+                    hasKey[c-'a'] = true;
                 }
             }
-
-            doors = new ArrayList[26];
-            for (int i = 0; i < 26; i++) doors[i] = new ArrayList<>();
-
-            visited = new boolean[h + 2][w + 2];
 
             sb.append(bfs()).append('\n');
         }
+        System.out.println(sb);
 
-        System.out.print(sb);
     }
-
-    static int bfs() {
+    static int bfs(){
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0, 0});
+        visited = new boolean[n+2][m+2];
+
         visited[0][0] = true;
+        q.offer(new int[]{0,0});
 
-        int documents = 0;
+        List<int[]>[] door = new ArrayList[26];
+        for(int i=0;i<26;i++){
+            door[i] = new ArrayList<>();
+        }
+        cnt = 0;
 
-        while (!q.isEmpty()) {
+        while (!q.isEmpty()){
             int[] cur = q.poll();
-            int y = cur[0];
-            int x = cur[1];
+            int py = cur[0];
+            int px = cur[1];
 
-            for (int d = 0; d < 4; d++) {
-                int ny = y + dy[d];
-                int nx = x + dx[d];
+            for(int i=0; i<4; i++){
+                int ny = py+dx[i];
+                int nx = px+dy[i];
 
-                if (ny < 0 || nx < 0 || ny >= h + 2 || nx >= w + 2) continue;
-                if (visited[ny][nx] || map[ny][nx] == '*') continue;
-
-                char c = map[ny][nx];
-
-                // 문
-                if ('A' <= c && c <= 'Z') {
-                    int idx = c - 'A';
-                    if (!hasKey[idx]) {
-                        doors[idx].add(new int[]{ny, nx});
+                if(ny<0 || ny>=n+2 || nx<0 || nx>=m+2 || arr[ny][nx]=='*' || visited[ny][nx]) continue;
+                //문
+                if('A'<=arr[ny][nx] && arr[ny][nx] <= 'Z'){
+                    if(!hasKey[arr[ny][nx]-'A']){
+                        door[arr[ny][nx]-'A'].add(new int[]{ny,nx});
                         continue;
                     }
                 }
-
-                // 열쇠
-                if ('a' <= c && c <= 'z') {
-                    int idx = c - 'a';
-                    if (!hasKey[idx]) {
-                        hasKey[idx] = true;
-                        for (int[] door : doors[idx]) {
-                            q.offer(door);
+                //열쇠
+                if('a'<=arr[ny][nx] && arr[ny][nx] <= 'z'){
+                    if(!hasKey[arr[ny][nx]-'a']){
+                        hasKey[arr[ny][nx]-'a'] = true;
+                        for(int[] d: door[arr[ny][nx]-'a']){
+                            q.offer(d);
+                            visited[d[0]][d[1]] = true;
                         }
-                        doors[idx].clear();
                     }
                 }
-
-                // 문서
-                if (c == '$') {
-                    documents++;
-                }
+                //문서
+                if(arr[ny][nx]=='$') cnt++;
 
                 visited[ny][nx] = true;
-                q.offer(new int[]{ny, nx});
+                q.offer(new int[]{ny,nx});
+
             }
         }
-
-        return documents;
+        return cnt;
     }
 }
