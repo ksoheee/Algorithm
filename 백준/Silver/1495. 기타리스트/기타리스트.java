3b4a16ff61ccbe9e9ca1e5802b2aec467e4cc20b
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-12-18 17:27:12 +0900

    [Silver I] Title: 기타리스트, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/1495. 기타리스트/기타리스트.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n+1];
+        st = new StringTokenizer(br.readLine()," ");
+        for(int i=1; i<=n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        boolean[][] dp = new boolean[n+1][m+1];
+        dp[0][s]=true;
+
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<=m; j++){
+                if(dp[i-1][j]){
+                    if(j+arr[i]<=m){
+                        dp[i][j+arr[i]] = true;
+                    }
+                    if(j-arr[i]>=0){
+                        dp[i][j-arr[i]] = true;
+                    }
+                }
+            }
+        }
+
+        int max = -1;
+        for(int i=m; i>=0; i--){
+            if(dp[n][i]){
+                max = i;
+                break;
+            }
+        }
+        System.out.println(max);
+    }
+}
