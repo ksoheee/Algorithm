2e157956add92ad32382881f7276dc9a90512565
ksoheee <83942772+ksoheee@users.noreply.github.com>
2025-12-31 18:57:49 +0900

    [Gold V] Title: 로봇 청소기, Time: 104 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -25,76 +25,41 @@ class Main{
             }
         }
 
-        int cnt = 1;
-        //처음 청소기 위치 청소
-        room[robotR][robotC] = 2;
+        //햔제 칸에서 상하좌우에 빈칸 있으면 90도 회전하고 빈칸일때 앞으로 전진
+        int cnt = 0;
         while(true){
-            int nclean = 0;
+            //현재칸 청소
+            if(room[robotR][robotC] == 0){
+               room[robotR][robotC] = 2;
+               cnt++;
+            }
 
-            int nr = robotR, nc = robotC, nd = robotD;
-
-            //현재칸 주변 4칸중 청소되지 않은 칸이 있는지 확인
+            //현재 칸에서 4방향 탐색, 90도 회전하면서 청소할 수 있는 칸 있으면 청소
+            boolean moved = false;
             for(int i=0; i<4; i++){
-                nr = robotR+dy[i];
-                nc = robotC+dx[i];
-                if(nr>=0 && nc>=0 && nr<n && nc<m && room[nr][nc]==0){
+                robotD = (robotD-1+4)%4;
+                int nr = robotR + dy[robotD];
+                int nc = robotC + dx[robotD];
+
+                if(room[nr][nc] == 0){
+                    robotR = nr;
+                    robotC = nc;
+                    moved = true;
                     break;
                 }
-                nclean++;
             }
+            if(moved) continue;
 
-            //빈칸이 없는 경우 후진
-            if(nclean==4){
-                if(robotD==0){
-                   nr = robotR+1;
-                   nc = robotC;
-                }else if(robotD==1){
-                    nr = robotR;
-                    nc = robotC-1;
-                }else if(robotD==2){
-                    nr = robotR-1;
-                    nc = robotC;
-                }else if(robotD==3){
-                    nr = robotR;
-                    nc = robotC+1;
-                }
-                //뒤로 갔는데 벽이면 끝
-                if(room[nr][nc]==1) break;
-                //벽이 아니면 청소 안되있으면 청소 후 빈칸있는지 탐색
-                //청소 되어있으면 탐색만
-                if(room[nr][nc]==0) {
-                    room[nr][nc] = 2;
-                    cnt++;
-                }
-                robotR = nr;
-                robotC = nc;
-            }
-            //빈칸이 있는 경우
-            else{
-                //빈칸을 찾을때까지 90도 회전
-                for(int i=0; i<4; i++){
-                    if(nd==0){
-                        nd = 3;
-                    }else if(nd==1){
-                        nd = 0;
-                    }else if(nd==2){
-                        nd = 1;
-                    }else if(nd==3){
-                        nd = 2;
-                    }
-                    nr = robotR + dy[nd];
-                    nc = robotC + dx[nd];
+            //빈칸 없었을 때 후진
+            int bd = (robotD+2)%4;
+            int _br = robotR+dy[bd];
+            int _bc = robotC+dx[bd];
 
-                    if(room[nr][nc]==0){
-                        room[nr][nc] = 2;
-                        cnt++;
-                        robotR = nr;
-                        robotC = nc;
-                        robotD = nd;
-                        break;
-                    }
-                }
-            }
+            if(room[_br][_bc] == 1) break;
+
+            robotR = _br;
+            robotC = _bc;
+
         }
         System.out.println(cnt);
 
