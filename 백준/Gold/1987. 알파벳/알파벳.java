2d93456af856390bab8ec066b23325b4c8b2e204
ksoheee <83942772+ksoheee@users.noreply.github.com>
2026-01-14 18:24:04 +0900

    [Gold IV] Title: 알파벳, Time: 792 ms, Memory: 14672 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -5,10 +5,8 @@ public class Main {
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
     static char[][] board;
-    static boolean[][] visited;
     static int N,M;
     static int max;
-    static boolean[] alphabet = new boolean[26];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
@@ -17,20 +15,17 @@ public class Main {
 
         //입력
         board = new char[N][M];
-        visited = new boolean[N][M];
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
             for(int j = 0; j < M; j++) {
                 board[i][j] = str.charAt(j);
             }
         }
-        int idx =board[0][0] - 'A';
-        alphabet[idx] = true;
-
-        dfs(1,0,0);
+        int mask = 1<<(board[0][0] - 'A');
+        dfs(1,mask,0,0);
         System.out.println(max);
     }
-    static void dfs(int dep, int r, int c){
+    static void dfs(int dep, int mask, int r, int c){
         max = Math.max(max, dep);
 
         for(int i = 0; i < 4; i++){
@@ -39,10 +34,9 @@ public class Main {
 
             if(nr<0 || nr>=N || nc<0 || nc>=M) continue;
             int idx = board[nr][nc]-'A';
-            if(alphabet[idx]) continue;
-            alphabet[idx] = true;
-            dfs(dep+1, nr, nc);
-            alphabet[idx] = false;
+            //이미 사용한 알파벳인지 확인
+            if((mask & (1<<idx)) != 0) continue;
+            dfs(dep+1,mask | (1<<idx),nr,nc);
         }
     }
 }
