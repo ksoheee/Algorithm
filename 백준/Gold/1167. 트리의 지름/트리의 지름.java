593cca1d8fe296dd738f61f5dcd7a729b0f0b6c6
ksoheee <83942772+ksoheee@users.noreply.github.com>
2026-02-12 14:09:45 +0900

    [Gold II] Title: 트리의 지름, Time: 672 ms, Memory: 98660 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -1,85 +1,70 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    static class Edge {
-        int to, w;
-        Edge(int to, int w) {
-            this.to = to;
+    static class Node{
+        int t, w;
+        public Node(int t, int w){
+            this.t = t;
             this.w = w;
         }
     }
 
-    static List<Edge>[] g;
-
-    static class Pair {
-        int node;
-        long dist;
-        Pair(int node, long dist) {
-            this.node = node;
-            this.dist = dist;
-        }
-    }
-
-    static Pair bfs(int start, int n) {
-        boolean[] vis = new boolean[n + 1];
-        long[] dist = new long[n + 1];
-        Arrays.fill(dist, -1);
-
-        ArrayDeque<Integer> q = new ArrayDeque<>();
-        q.add(start);
-        vis[start] = true;
-        dist[start] = 0;
-
-        int farNode = start;
-        long farDist = 0;
-
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-
-            for (Edge e : g[cur]) {
-                if (!vis[e.to]) {
-                    vis[e.to] = true;
-                    dist[e.to] = dist[cur] + e.w;
-                    q.add(e.to);
-
-                    if (dist[e.to] > farDist) {
-                        farDist = dist[e.to];
-                        farNode = e.to;
-                    }
-                }
-            }
-        }
-        return new Pair(farNode, farDist);
-    }
-
+    static List<Node>[] nodes;
+    static boolean[] visited;
+    static int farNode;
+    static int maxNum;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int n  = Integer.parseInt(br.readLine());
 
-        g = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) g[i] = new ArrayList<>();
+        nodes = new ArrayList[n+1];
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 1; i <= n; i++) nodes[i] = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int nodeNum = Integer.parseInt(st.nextToken());
 
-            while (true) {
+            while(true){
                 int connectionNum = Integer.parseInt(st.nextToken());
-                if (connectionNum == -1) break;
-                int w = Integer.parseInt(st.nextToken());
+                if(connectionNum == -1) break;
+                int weight = Integer.parseInt(st.nextToken());
+                nodes[nodeNum].add(new Node(connectionNum, weight));
 
-                g[nodeNum].add(new Edge(connectionNum, w));
             }
         }
+        //한개의 노선 골라서 그 노선에서 가장 먼 노선 찾기
+        farNode = 1;
+        maxNum = -1;
+        visited = new boolean[n+1];
+        dfs(1,0);
 
-        // 1) 아무 노드(1)에서 가장 먼 노드 A 찾기
-        Pair first = bfs(1, n);
 
-        // 2) A에서 가장 먼 거리 = 트리의 지름
-        Pair second = bfs(first.node, n);
 
-        System.out.println(second.dist);
+        //그 노선에서 가장 먼 노선 찾기
+        maxNum = -1;
+        visited = new boolean[n+1];
+        dfs(farNode,0);
+
+        System.out.println(maxNum);
+
+
     }
+    static void dfs(int cur, int dist){
+        visited[cur] = true;
+
+        if(maxNum <dist){
+            maxNum = dist;
+            farNode = cur;
+        }
+
+        for(Node node: nodes[cur]){
+            if(!visited[node.t]){
+                dfs(node.t, dist+node.w);
+            }
+        }
+    }
+
+
 }
