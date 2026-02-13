e28ec5a717419e156b42829edd4b8844624e96a6
ksoheee <83942772+ksoheee@users.noreply.github.com>
2026-02-13 19:21:50 +0900

    [Gold IV] Title: LCS 3, Time: 160 ms, Memory: 20624 KB -BaekjoonHub

---
+++ 백준/Gold/1958. LCS 3/LCS 3.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String A = br.readLine();
+        String B = br.readLine();
+        String C = br.readLine();
+
+        int[][][] dp = new int[A.length()+1][B.length()+1][C.length()+1];
+
+        for (int i = 1; i <= A.length(); i++) {
+            for (int j = 1; j <= B.length(); j++) {
+                for (int k = 1; k <= C.length(); k++) {
+                    if (A.charAt(i-1) == B.charAt(j-1) &&
+                            B.charAt(j-1) == C.charAt(k-1)) {
+
+                        dp[i][j][k] = dp[i-1][j-1][k-1] + 1;
+                    } else {
+                        dp[i][j][k] = Math.max(
+                                dp[i-1][j][k],
+                                Math.max(
+                                        dp[i][j-1][k],
+                                        dp[i][j][k-1]
+                                )
+                        );
+                    }
+                }
+            }
+        }
+        System.out.println(dp[A.length()][B.length()][C.length()]);
+    }
+
+}
