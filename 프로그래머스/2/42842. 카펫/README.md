356103610b608dd83f057f455591fa3cb0ae180d
ksoheee <83942772+ksoheee@users.noreply.github.com>
2026-04-02 18:46:03 +0900

    [level 2] Title: 카펫, Time: 17.31 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
 
 ### 성능 요약
 
-메모리: 84.9 MB, 시간: 0.04 ms
+메모리: 91.3 MB, 시간: 17.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 24일 23:26:59
+2026년 04월 02일 18:45:59
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,19 +1,35 @@
+import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
-        for (int h = 1; h * h <= yellow; h++) {
-            if (yellow % h != 0) {
-                continue;
-            }
-            int innerH = h;
-            int innerW = yellow / h;
-
-            int carpetH = innerH + 2;
-            int carpetW = innerW + 2;
-
-            if (carpetW * carpetH == brown + yellow) {
-                return new int[]{carpetW, carpetH};
+        //가로>=세로, 세로>=3
+        //테두리만 갈색
+        //노랑색이 소수인 경우에 노랑의 세로는 1
+        
+        List<Integer> divisor = new ArrayList<>();
+        List<int[]> muls = new ArrayList<>();
+        //약수 구하기
+        for(int i=yellow; i>0; i--){
+            if(yellow % i == 0) divisor.add(i);
+        }
+        //약수 중 큰수 부터 약수 * x = 노랑이 되는 짝을 찾아서 리스트에 저장
+        for(int i : divisor){
+            int j = yellow/i;
+            if(j>i) break;
+            muls.add(new int[]{i,j});
+        }
+        
+        List<Integer> answer = new ArrayList<>();
+        // 갈색(노랑가로+2, 노랑세로+2)
+        for(int[] mul : muls){
+            int b1 = mul[0]+2;
+            int b2 = mul[1]+2;
+            
+            if((b1*b2)-(mul[0]*mul[1])==brown){
+                answer.add(b1);
+                answer.add(b2);
+                break;
             }
         }
-        return new int[0];
+        return answer.stream().mapToInt(Integer::intValue).toArray(); 
     }
 }
