45d466f862ee3857e8a77ed7e62dcf3f52e8c62b
ksoheee <83942772+ksoheee@users.noreply.github.com>
2026-08-18 21:16:32 +0900

    [level 2] Title: 스킬트리, Time: 0.18 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/README.md
+++ 프로그래머스/2/49993. 스킬트리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 스킬트리 - 49993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993#) 
 
 ### 성능 요약
 
-메모리: 86.5 MB, 시간: 0.27 ms
+메모리: 74.1 MB, 시간: 0.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 04일 16:37:57
+2026년 08월 18일 21:16:26
 
 ### 문제 설명
 
--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,29 +1,33 @@
 import java.util.*;
+
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-        int len = skill.length();
+        int n = skill_trees.length;
         
-        List<Character> check;;
+        char[] skillArr = skill.toCharArray();
         
-        for(int i=0; i<skill_trees.length; i++){
-            check = new ArrayList<>();
-            for(int j=0; j<skill_trees[i].length(); j++){
-                for(int k=0; k<len; k++){
-                    if(skill_trees[i].charAt(j)==skill.charAt(k)) {
-                        check.add(skill_trees[i].charAt(j));
-                    }    
-                }
-            }
+        for(int i=0; i<n; i++){
+            Deque<Character> check = new ArrayDeque<>();
             
-            boolean flag = true;
-            for(int j=0; j<check.size(); j++){
-                if(skill.charAt(j) != check.get(j)){
-                    flag=false;
-                    break; 
-                } 
+            //스킬만 삽입
+            for(int j=0; j<skill_trees[i].length(); j++){
+                for(int k=0; k<skill.length(); k++){
+                    if(skill_trees[i].charAt(j)==skill.charAt(k)){
+                        check.offerLast(skill_trees[i].charAt(j));
+                    }
+                }
+                
             }
-            if(flag) answer++;
+            //판단
+            int size = check.size();
+            for(int k=0; k<size; k++){
+                if(skillArr[k] == check.getFirst()){
+                    check.pollFirst();
+                }
+                //if(skillArr[k] != check.pollFirst()) break;
+            }
+            if(check.isEmpty()) answer++;
         }
         return answer;
     }
